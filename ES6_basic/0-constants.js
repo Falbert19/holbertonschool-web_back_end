95860e2e9faad1256fbf11f956fc4e64f38feaba
Jesus A <blackyizus@YIZUS-MAC.local>
2025-03-29 19:23:09 -0400

    task 0 redo

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,14 +1,26 @@
+/**
+ * Returns a constant task string.
+ * @return {string} The task string.
+ */
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
+/**
+ * Returns a fixed string.
+ * @return {string} The last part of the sentence.
+ */
 export function getLast() {
-    return ' is okay';
+  return ' is okay';
 }
 
+/**
+ * Returns a sentence with a variable.
+ * @return {string} A sentence using let.
+ */
 export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-    return combination;
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
 }
