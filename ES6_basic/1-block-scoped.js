366d16979520b33cc0cacf29dd21cdac9993c983
Jesus A <blackyizus@YIZUS-MAC.local>
2025-03-29 19:46:12 -0400

    task 1 redo

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,15 @@
+/**
+ * Ensures block-scoped variables are not overwritten.
+ * @param {boolean} trueOrFalse - Condition to execute the block.
+ * @return {boolean[]} The values of task and task2.
+ */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    console.log('Inside block scope');
   }
+
+  return [task, task2];
+}
