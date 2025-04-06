3dfc038cf6eff323f7c1c88785eed478f8879838
Falbert19 <fernandoalbert19@gmail.com>
2025-04-06 14:36:39 -0400

    lint passed

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-        const gradeRecord = newGrades.find((g) => g.studentId === student.id);
+      const gradeRecord = newGrades.find((g) => g.studentId === student.id);
         return {
-            ...student,
-            grade: gradeRecord ? gradeRecord.grade : 'N/A',
+          ...student,
+          grade: gradeRecord ? gradeRecord.grade : 'N/A',
         };
     });
 }
