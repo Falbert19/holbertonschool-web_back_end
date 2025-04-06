e9fb7c6c2ad2b42a7e58b8c19032d0cbde9531b1
Falbert19 <fernandoalbert19@gmail.com>
2025-04-06 15:53:23 -0400

    task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceries_list() {
+  const items = [
+    ['Apple', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  return new Map(items);
+}
