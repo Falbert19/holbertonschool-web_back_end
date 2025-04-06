88b8bae7dc4e3fcc45a112f4c8cb7618fa731b96
Falbert19 <fernandoalbert19@gmail.com>
2025-04-06 15:40:07 -0400

    lint changes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -6,6 +6,6 @@ export default function cleanSet(set, startString) {
   const filtered = [...set]
     .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length));
-    
+
   return filtered.join('-');
 }
