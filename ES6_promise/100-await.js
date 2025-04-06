dd65820bafa2d52403d114cddf03d6a0503c8c5f
Falbert19 <fernandoalbert19@gmail.com>
2025-04-06 19:39:36 -0400

    task 100

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+import uploadPhoto from './5-photo-reject';
+import createUser from './utils.js';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user }
+  } catch (error) {
+    return { photo: null, user: null };
+  } 
+}
