9523452bd65663832673a9a5d587a986abea67e4
Falbert19 <fernandoalbert19@gmail.com>
2025-04-06 22:54:10 -0400

    lint changed 5 again

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,11 +4,11 @@ export default class Building {
       throw new TypeError('sqft must be a number');
     }
 
-  this._sqft = sqft;
+    this._sqft = sqft;
 
-  if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
