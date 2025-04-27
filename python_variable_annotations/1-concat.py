bbdc66f82ad2e9a41c48bc5218d9dc6a5097c7e7
Falbert19 <fernandoalbert19@gmail.com>
2025-04-27 18:35:34 -0400

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module is for basic type=annoted concat function """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This return the conceted of 2 strings """
+    return str1 + str2
