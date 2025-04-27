7b8255586cf641bfd14a16860f24b42203cca9ff
Falbert19 <fernandoalbert19@gmail.com>
2025-04-27 18:49:55 -0400

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" this module is for basic type-annotated to_str function """
+
+
+def to_str(n: float) -> str:
+    """ This return the string of a float """
+    return str(n)
