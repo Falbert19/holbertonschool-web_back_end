b58682c2ab370d529fad3f278fc6217a9423c3b9
Falbert19 <fernandoalbert19@gmail.com>
2025-04-27 18:44:50 -0400

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" this module is for basic type-annotated  floor function """
+
+import math
+
+
+def floor(n: float) -> int:
+    """ this return the floor of a floatt """
+    return math.floor(n)
