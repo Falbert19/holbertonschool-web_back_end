4bc538c1f1e6016c70ce76d32a4ab008db6b5e2f
Falbert19 <fernandoalbert19@gmail.com>
2025-05-04 23:47:22 -0400

    task 0 redo

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 async_generator that takes no arguments"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ yield a random num between 0 and 10 after 1 sec of waiting """
-    for g in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
