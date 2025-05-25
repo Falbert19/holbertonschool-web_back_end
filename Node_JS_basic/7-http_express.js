3e7f3527bbcf649c4fb7db320b59531f11777bb4
Fernando L. Albert <fernandoalbert19@gmail.com>
2025-05-25 18:54:27 -0400

    task 7 changes

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,7 +1,6 @@
 /*eslint-disable*/
 const express = require('express');
 const countStudents = require('./3-read_file_async');
-const e = require('express');
 
 const app = express();
 const database = process.argv[2];
@@ -10,18 +9,18 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async(req, res) => {
-    let response = 'This is the list of our students\n';
-    try {
-        const data = await countStudents(database);
-       response += `\n${data}`;
-       res.set('Content-Type', 'text/plain');
-       res.send(response);
-    } catch (err) {
-        response += `\n${err.message}`;
-        res.set('Content-Type', 'text/plain');
-        res.send(response);
-    }
+app.get('/students', async (req, res) => {
+  let response = 'This is the list of our students';
+  try {
+    const data = await countStudents(database);
+    response += `\n${data.trim()}`;  // ✅ Removes extra newline from start or end
+    res.set('Content-Type', 'text/plain');
+    res.send(response);
+  } catch (err) {
+    response += `\n${err.message}`;
+    res.set('Content-Type', 'text/plain');
+    res.send(response);
+  }
 });
 
 app.listen(1245);
