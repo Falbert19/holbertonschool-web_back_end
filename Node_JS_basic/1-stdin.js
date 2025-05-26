ca6c836cae2908388f8e2d115c92dc8ea0f2ae1e
Fernando L. Albert <fernandoalbert19@gmail.com>
2025-05-25 21:51:51 -0400

    task 1 redo

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,6 +4,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+});
+
+// Handle end of input (e.g., when piped or Ctrl+D)
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
